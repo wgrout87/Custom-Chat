1b93919abd6c10876dd66c0e4f55cba1418d3207
WGrout <wgrout87@gmail.com>
2023-11-01 16:54:54 -0600

    user can input text into test function

--- ff/ff.js
+++ ff/ff.js
@@ -1,4 +1,4 @@
-let totalMessages = 0, messagesLimit = 0, removeSelector, addition, channelName, provider, version, fontSize, usernameRatio, scale, characterHeight, characterWidth, typewriter = 'true', typewriterSpeed = 100;
+let totalMessages = 0, messagesLimit = 0, removeSelector, addition, channelName, provider, version, fontSize, usernameRatio, scale, characterHeight, characterWidth, typewriter = 'true', typewriterSpeed = 100, testMessageText;
 let animationIn = 'bounceIn';
 let animationOut = 'bounceOut';
 let hideAfter = 60;
@@ -120,7 +120,7 @@ window.addEventListener('onEventReceived', function (obj) {
                                 description: "Verified"
                             }],
                             channel: channelName,
-                            text: "Howdy! My name is Bill and I am here to serve Kappa",
+                            text: testMessageText,
                             isAction: !1,
                             emotes: [{
                                 type: "twitch",
@@ -188,6 +188,7 @@ window.addEventListener('onWidgetLoad', function (obj) {
     characterWidth = fontSettings[version].characterWidth;
     typewriter = fieldData.typewriter;
     typewriterSpeed = fieldData.typewriterSpeed;
+    testMessageText = fieldData.testMessageText;
     getFontCoordinatesObj(characterHeight, characterWidth);
     fetch('https://api.streamelements.com/kappa/v2/channels/' + obj.detail.channel.id + '/').then(response => response.json()).then((profile) => {
         provider = profile.provider;
